cca3a0e7862cdc0bb240d4463a6ba4afbf11a654
Vladislav Rudenko <vladisrud@gmail.com>
2024-02-28 23:30:33 +0400

    Make easier without additional StackView, try to put button right

--- Signal/ViewController.swift
+++ Signal/ViewController.swift
@@ -10,14 +10,40 @@ import UIKit
 class ViewController: UIViewController {
     
     //MARK: - UI Elements
-    private var overallStack: UIStackView = {
+    private var signalStack: UIStackView = {
         let stack = UIStackView()
         stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .vertical
-        stack.spacing = 10
-        stack.distribution = .fillProportionally
+        stack.distribution = .equalSpacing
         stack.alignment = .center
         return stack
+    }()
+    
+    let redSignal: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
+        view.layer.cornerRadius = 50
+        view.alpha = 0.3
+        return view
+    }()
+    
+    let yellowSignal: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .yellow
+        view.layer.cornerRadius = 50
+        view.alpha = 0.3
+        return view
+    }()
+    
+    let greenSignal: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .green
+        view.layer.cornerRadius = 50
+        view.alpha = 0.3
+        return view
     }()
     
     let controlButton: UIButton = {
@@ -32,48 +58,13 @@ class ViewController: UIViewController {
         return button
     }()
     
-    private var signalStack: UIStackView = {
-        let stack = UIStackView()
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        stack.axis = .vertical
-        stack.spacing = 35
-        stack.distribution = .fillEqually
-        stack.alignment = .center
-        return stack
-    }()
-    
-    let redSignal: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .red
-        view.alpha = 0.3
-        return view
-    }()
-    
-    let yellowSignal: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .yellow
-        view.alpha = 0.3
-        return view
-    }()
-    
-    let greenSignal: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .green
-        view.alpha = 0.3
-        return view
-    }()
-    
     //MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.addSubview(overallStack)
         
-        overallStack.addArrangedSubview(signalStack)
-        overallStack.addArrangedSubview(controlButton)
+        view.addSubview(signalStack)
+        view.addSubview(controlButton)
         
         signalStack.addArrangedSubview(redSignal)
         signalStack.addArrangedSubview(yellowSignal)
@@ -87,26 +78,25 @@ class ViewController: UIViewController {
     //MARK: - Constrains UI Elements
     func doContrains() {
         NSLayoutConstraint.activate([
-            overallStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            overallStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            overallStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            overallStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            signalStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            signalStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            signalStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            signalStack.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.5),
             
-//            signalStack.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5),
+            controlButton.topAnchor.constraint(lessThanOrEqualTo: signalStack.bottomAnchor, constant: 100),
+            controlButton.leadingAnchor.constraint(lessThanOrEqualTo: view.leadingAnchor, constant: 100),
+            controlButton.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: 100),
+            controlButton.bottomAnchor.constraint(lessThanOrEqualTo: view.bottomAnchor, constant: 100),
             
-            redSignal.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
-            redSignal.heightAnchor.constraint(equalTo: redSignal.widthAnchor),
-            yellowSignal.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
-            yellowSignal.heightAnchor.constraint(equalTo: yellowSignal.widthAnchor),
-            greenSignal.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
-            greenSignal.heightAnchor.constraint(equalTo: greenSignal.widthAnchor),
+            redSignal.widthAnchor.constraint(equalToConstant: 100),
+            redSignal.heightAnchor.constraint(equalToConstant: 100),
+            yellowSignal.widthAnchor.constraint(equalToConstant: 100),
+            yellowSignal.heightAnchor.constraint(equalToConstant: 100),
+            greenSignal.widthAnchor.constraint(equalToConstant: 100),
+            greenSignal.heightAnchor.constraint(equalToConstant: 100),
             
 //            controlButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5)
         ])
-        
-        redSignal.layer.cornerRadius = view.frame.size.width / 2 / 2
-        yellowSignal.layer.cornerRadius = view.frame.size.width / 2 / 2
-        greenSignal.layer.cornerRadius = view.frame.size.width / 2 / 2
         
     }
     
